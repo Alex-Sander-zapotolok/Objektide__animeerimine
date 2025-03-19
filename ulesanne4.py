7409ea66f8111a2c94e3aface6a5fc90d0feca36
alex.zapotolok <alex.zapotolok@vikk.ee>
2025-03-19 12:30:09 +0200

    Ülesanne 4 tehtud

--- ulesanne4.py
+++ ulesanne4.py
@@ -1,16 +1,27 @@
-import pygame
 import sys
 
-screenX = 640
-screenY = 480
-screen = pygame.display.set_mode([screenX, screenY])
-pygame.display.set_caption("Animeerimine")
+import pygame
+pygame.init()
 
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Formula 1 võidusõidu möng')
 clock = pygame.time.Clock()
 
 taust = pygame.image.load("bg_rally.jpg")
 punane_auto = pygame.image.load("f1_red.png")
 sinine_auto = pygame.image.load("f1_blue.png")
+sinine_auto2 = pygame.image.load("f1_blue.png")
+sinine_auto3 = pygame.image.load("f1_blue.png")
+
+speed = 3
+skoor = 0
+
+sin_auto = 32
+sin_auto2 = 147
+sin_auto3 = 87
+
 
 gameover = False
 while not gameover:
@@ -21,8 +32,37 @@ while not gameover:
         if event.type == pygame.QUIT:
             gameover = True
 
+    if sin_auto > 480:
+        skoor += 1
+        sin_auto = 0
+
+    if sin_auto2 > 480:
+        skoor += 1
+        sin_auto2 = 0
+
+    if sin_auto3 > 480:
+        skoor += 1
+        sin_auto3 = 0
+
+
+
+
+    sin_auto += speed
+    sin_auto2 += speed
+    sin_auto3 += speed
+
     screen.blit(taust, (0, 0))
     screen.blit(punane_auto, (297.5, 391))
+    screen.blit(sinine_auto, (175, sin_auto))
+    screen.blit(sinine_auto2, (297.5, sin_auto2))
+    screen.blit(sinine_auto3, (415, sin_auto2))
+
+    font = pygame.font.Font(None, 30)
+    text = font.render("Formula 1 skoori punktid: " + str(skoor), True, [0, 0, 0])
+    screen.blit(text, [200, 0])
+
+
+
     pygame.display.flip()
 
 pygame.quit()
